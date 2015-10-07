835740a1564ff4eae9bbc6075f99e581bc4ae151
Mg <gamem.8818@gmail.com>
2015-10-07 09:46:10 -0700

    update package

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var engines = require('consolidate');
 var app = new express();
 var http = require('http');
 app.set('port', process.env.PORT || 9000);
-app.use(express.static(__dirname + '/testpage'));
-//app.engine('html', engines.mustache);
-//app.set('view engine', 'html');
+app.set('views', __dirname + '/testpage');
+app.engine('html', engines.handlebars);
+app.set('view engine', 'html');
 
 app.get('/', function(req, res) {
     res.render('testpage.html');
